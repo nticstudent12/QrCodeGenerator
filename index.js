b4a37c8196adce95412ab48c933565258e7d8c83
nticstudent12 <153958282+nticstudent12@users.noreply.github.com>
2024-12-07 16:24:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,3 @@ button.onclick = () => {
     qr.style = "width: 89%;background-color: #eee;height: 220px;margin-bottom: 20px;border-radius:15px;margin-left:15px;"
     qrimg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ input.value;
 }
-
-
